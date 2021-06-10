6e345316914fd09429e03ebd74bce0fb82bb8cec
hinker <imgcre@outlook.com>
2021-06-10 06:03:44 -0900

    (test)成员函数requires测试

--- tests/temp.whatever.cxx
+++ tests/temp.whatever.cxx
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+template<class F, class T>
+concept MemberFuncOf = requires(T t, F f) {
+    (t.*f)();
+};
+
+struct Test {
+    void f() {
+
+    };
+};
+
+void f() {
+
+}
+
 void test_except() {
-    
+    rt_kprintf("Test::f: %s\n", typeid(&Test::f).name());
+    auto f = &Test::f;
+    auto t = Test{};
+    (t.*f)();
+    rt_kprintf("Test::f: %d\n", MemberFuncOf<Test, decltype(&Test::f)>);
+    rt_kprintf("f: %d\n", MemberFuncOf<Test, decltype(&f)>);
 }
 
 MSH_CMD_EXPORT(test_except, );
