79e726620a0a2d0af2b86a64a779e18c46972a4e
hinker <imgcre@outlook.com>
2021-06-12 10:35:54 -0900

    (feat)串口(接收)

--- hal/uart.cxx
+++ hal/uart.cxx
@@ -1,5 +1,31 @@
 #include "uart.hxx"
+#include <stdexcept>
+#include <rthw.h>
+#include <os/timeout.hxx>
 
-// Uart::Uart(const char* deviceName, struct serial_configure* conf = nullptr) {
+Uart::Uart(const char* deviceName, struct serial_configure* conf) {
+    device = rt_device_find(deviceName);
+    if(conf != nullptr)
+        rt_device_control(device, RT_DEVICE_CTRL_CONFIG, conf);
+    rt_device_open(device, RT_DEVICE_FLAG_INT_TX);
+    Indicator::setup(device, &Uart::onRecv);
+}
 
-// }
+int Uart::read(std::byte* data, int len, std::chrono::milliseconds timeout) {
+    auto helper = Timeout{timeout};
+    while(rxRemain < len) {
+        event.wait(helper.remain());
+    }
+    rxRemain -= len;
+    rt_device_read(device, 0, data, len);
+    return len;
+}
+
+void Uart::write(std::byte* data, int len) {
+    
+}
+
+void Uart::onRecv(std::size_t size) {
+    event.set();
+    rxRemain = size;
+}
--- hal/uart.hxx
+++ hal/uart.hxx
@@ -1,7 +1,23 @@
 #pragma once
 #include <dp/iostream.hxx>
+#include <rx_indicator.hxx>
+#include <rtthread.h>
+#include <os/event_allocator.hxx>
+#include <atomic>
 
-// class Uart: public IOStream {
-//   public:
-//     Uart(const char* deviceName, struct serial_configure* conf = nullptr);
-// };
+class Uart: public IOStream, public RxIndicator<Uart> {
+  public:
+    Uart(const char* deviceName, struct serial_configure* conf = nullptr);
+    Uart(const Uart&) = delete;
+
+    //read all or read none
+    virtual int read(std::byte* data, int len, std::chrono::milliseconds timeout) override;
+    virtual void write(std::byte* data, int len) override;
+  private:
+    void onRecv(std::size_t size);
+
+  private:
+    rt_device_t device;
+    Event event = EventAllocator::alloc();
+    std::atomic<std::size_t> rxRemain = 0;
+};
